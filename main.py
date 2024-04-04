03c01c73db3cc18d3667f8748d3f372eba0c8dc3
saci zakaria <sacideveloppeur@gmail.com>
2024-04-04 10:17:08 +0200

    prompt

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from langchain.llms import OpenAI
 api_key = "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"
 
 llm = OpenAI(
-    openai_api_key=api_key,
-
-                       
+    openai_api_key=api_key,                   
     )
+llm("What is the meaning of life?")
