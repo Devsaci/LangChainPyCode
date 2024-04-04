d63e240f362c5dff643ad11e18a0a3304dcc21c7
saci zakaria <sacideveloppeur@gmail.com>
2024-04-04 10:16:05 +0200

    openai_api_key=api_key

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ from langchain.llms import OpenAI
 
 api_key = "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"
 
-llm = OpenAI(temperature=0, openai_api_key=api_key)
+llm = OpenAI(
+    openai_api_key=api_key,
+
+                       
+    )
