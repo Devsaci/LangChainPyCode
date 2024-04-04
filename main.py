ce3c361c8eaca2cc4910fd1346e1334e1b7cd85e
saci zakaria <sacideveloppeur@gmail.com>
2024-04-04 10:53:31 +0200

    code_prompt =

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ api_key = "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"
 llm = OpenAI(
     openai_api_key=api_key,                   
     )
+
+code_prompt = PromptTemplate(
+    
+)
 result = llm("What is the meaning of life?")
 print(result)
