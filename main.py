cdcd95fc28d505f390bc2111be795eecbe0b0dbb
saci zakaria <sacideveloppeur@gmail.com>
2024-04-04 10:51:29 +0200

    from langchain.prompts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from langchain.llms import OpenAI    # type: ignore
+from langchain.prompts import PromptTemplate  # type: ignore
 
 api_key = "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"
 
